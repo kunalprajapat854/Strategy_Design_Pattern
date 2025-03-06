30019ddd3f58d23b8a404e0762c0fc8f260a0f20
Kunal <virajprajapat854@gmail.com>
2025-03-06 19:57:28 +0530

    Performing Field Injection

--- src/in/kunal/BillCollector.java
+++ src/in/kunal/BillCollector.java
@@ -2,27 +2,28 @@ package in.kunal;
 
 public class BillCollector {
 
-	private IPayment payment;
-//    interface variable is not intialized to read the parameter value 
+	private IPayment payment;  // instance default value is null 
+	
+//	we call this variable it will return nullpointer exception
+//    interface variable is not initialized to read the parameter value 
 //    we need to use either setter method or constructor 
 
-	public void setPayment(IPayment payment) {
+	public void setPayment(IPayment payment) { // set the value for the payment variable
 		this.payment = payment;
 	}
 
 //	constructor method
-
 	public BillCollector(IPayment payment) {
 		this.payment = payment;
 	}
 	
-	public BillCollector() {
+	public BillCollector() { 
 		
 	}
 
 	public void paybill(double amount) {
 //	  CreditCardPayment  cardPayment = new CreditCardPayment();  //tightly coupled with credit class
-//	  cardPayment.pay(amount);
+//	  cardPayment.pay(amount); pay is reference to payment interface 
 		String pay = payment.pay(amount);
 		System.out.println(pay);
 
--- src/in/kunal/Test.java
+++ src/in/kunal/Test.java
@@ -1,18 +1,34 @@
 package in.kunal;
 
+import java.lang.reflect.Field;
+
 public class Test {
-	
-	public static void main(String[] args) {
-		
-		BillCollector billCollector = new BillCollector();
-//		using setter method to injecting creditcardPayment  into billcollector
-//		billCollector.setPayment(new UpiPayment());
-//		billCollector.paybill(1500.0);
-//		
-		//using constructor method injecting creditcard into billcollector
-		BillCollector billCollector2 = new BillCollector(new CreditCardPayment());
-		billCollector2.paybill(1500.0);
-		
+
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) throws Exception {
+
+//		BillCollector billCollector = new BillCollector();
+////		using [setter method] to injecting creditcardPayment  into billcollector
+////		billCollector.setPayment(new UpiPayment());
+////		billCollector.paybill(1500.0);
+////		
+//		//using [constructor method] injecting creditcard into billcollector
+//		BillCollector billCollector2 = new BillCollector(new CreditCardPayment());
+//		billCollector2.paybill(1500.0);
+
+		// Field Injection using interface ref variable
+
+		Class<?> clz = Class.forName("in.kunal.BillCollector"); // target class path
+		Field field = clz.getDeclaredField("payment"); // interface ref variable
+
+		field.setAccessible(true); // accessing private variable outside the class
+		Object object = clz.newInstance(); // creating the object of that variable
+
+		field.set(object, new UpiPayment()); // injecting dependent obj into billcollector
+
+		BillCollector billCollector = (BillCollector) object;  
+		billCollector.paybill(1500.0);
+
 	}
 
 }
